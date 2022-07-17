524e8e38f37226930e44e49fe9f48156c74eff88
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-17 11:44:57 +0300

    Домашнее задание

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -1,30 +1,66 @@
 package pro.sky.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pro.sky.calculator.CalculatorServiceImp;
+import pro.sky.calculator.service.CalculatorService;
 
 @RestController
-public class CalculatorController extends CalculatorServiceImp {
-    @GetMapping
-    public String hello() {
-        return "Добро пожаловать в калькулятор";
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping()
+    public String greetings() {
+        return "<h1>Добро пожаловать в калькулятор</h1>";
     }
     @GetMapping("/plus")
-    public int plus() {
-        return plus();
-    }
+    public String plus(@RequestParam(name = "num1", required = false) Integer a,
+                       @RequestParam(name = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Какой-то из параметров не передан";
+        }
+        return buildResultString(a, b, "+", calculatorService.plus(a, b));
+         }
     @GetMapping("/minus")
-    public int minus() {
-        return minus();
+    public String minus(@RequestParam(name = "num1", required = false) Integer a,
+                       @RequestParam(name = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Какой-то из параметров не передан";
+        }
+        return buildResultString(a, b, "-", calculatorService.minus(a, b));
     }
     @GetMapping("/multiply")
-    public int multiply() {
-        return multiply();
+    public String multiply(@RequestParam(name = "num1", required = false) Integer a,
+                       @RequestParam(name = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Какой-то из параметров не передан";
+        }
+        return buildResultString(a, b, "*", calculatorService.multiply(a, b));
     }
     @GetMapping("/divide")
-    public int divide() {
-        return divide();
+    public String divide(@RequestParam(name = "num1", required = false) Integer a,
+                       @RequestParam(name = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Какой-то из параметров не передан";
+        }
+        if (b == 0) {
+            return "Делить на ноль нельзя";
+        }
+        return buildResultString(a, b, "/", calculatorService.divide(a, b));
     }
+
+    private String buildResultString(int a,
+                                     int b,
+                                     String operation,
+                                     Number result) {
+//        return a + " " + operation + " " + b + " = " + result;
+        return String.format("%d %s %d=%s",a,operation,b,result);
+    }
+
 }
 
